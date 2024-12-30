b3428bad1c7969e5c914cfa573ce0f52e35921b0
Job Pulache <jobpulachecarrenojob837@gmail.com>
2024-12-30 11:57:39 -0500

    ending exercise two in comparison and logical

---
+++ comparisonAndLogical/02.js
@@ -0,0 +1,17 @@
+/*Comparando
+
+Utiliza la consola para escribir una expresión que compare dos strings y produzca un valor TRUE.
+Escribí una expresión comparando dos strings que devuelva FALSE.
+Escribí una expresión que involucre un string y un número y devuelva FALSE.
+Escribí una expresión que involucre un string y un número y devuelva TRUE*/
+
+let stringOne = prompt("Enter text.")
+let StringTwo = prompt("Enter textTwo")
+let number = parseInt(prompt("Enter the number"))
+console.log(stringOne != StringTwo);
+console.log(stringOne === StringTwo);
+console.log(stringOne === number);
+console.log(stringOne == number);
+
+
+
