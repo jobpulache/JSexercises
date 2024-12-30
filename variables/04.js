b7cc19649f7d150bcdf20e87a9dac9499f36338a
Job Pulache <jobpulachecarrenojob837@gmail.com>
2024-12-30 00:04:26 -0500

    ending four exercise

---
+++ variables/04.js
@@ -0,0 +1,7 @@
+/*Crea 2 variables numero y numerotriple.
+Pidele al usuario que ingrese el valor del numero a calcular mediante un prompt().
+Asigna a la variable numerotriple el valor de la multiplicación por 3 del numero ingresado.
+Muestralo el resultado por consola con el mensaje “El triple de este número es: (xx)”.*/
+let numero = parseInt(prompt("Enter the number"))
+let numeroTriple = numero * 3
+console.log(`the triple number of this number is ${numeroTriple}`);
