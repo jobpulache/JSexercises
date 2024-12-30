b6cced7bac421da0a38ba94c77c4160860c49e0a
Job Pulache <jobpulachecarrenojob837@gmail.com>
2024-12-30 00:08:20 -0500

    ending five exercise

---
+++ variables/05.js
@@ -0,0 +1,7 @@
+/*Para este ejercicio deberas googlear un poco. Busca en googlee como podemos saber el largo de los datos de una 
+variable en JS.
+Declara una variable que se llame texto y asignale el metodo prompt() para que le puedas solicitar al usuario que 
+ingrese un texto.
+Calcula su largo con lo que investigaste y muestra un alert() que diga “El largo del texto ingresado es: xx*/
+let texto = prompt("Enter the text")
+alert(`el largo del texto que ingresaste es: ${texto.length}`)
