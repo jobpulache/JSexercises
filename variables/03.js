f149f834124a54e417bda64041b77f91df0b54c8
Job Pulache <jobpulachecarrenojob837@gmail.com>
2024-12-29 23:57:22 -0500

    ending the three exercise

---
+++ variables/03.js
@@ -0,0 +1,8 @@
+/*Crea 3 variables numero1,numero2 y resultado.
+Pidele al usuario que ingrese los valores de las variables numero1 y numero2 mediante un prompt().
+Asigna a la variable resultado el valor de la suma entre numero1 y numero2.
+Muestra un alerta con el mensaje “El resultado es: (resultado)”.*/
+let n1 =parseInt(prompt("Enter first number"))
+let n2 = parseInt(prompt("Enter the second number"))
+let result = n1 + n2
+alert(`La suma de estos número es ${result}`)
