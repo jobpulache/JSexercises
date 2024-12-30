f211d6a2a791bd3f6c572e28cff9eee139e5a541
Job Pulache <jobpulachecarrenojob837@gmail.com>
2024-12-30 11:37:33 -0500

    ending sex exercise

---
+++ variables/06.js
@@ -0,0 +1,14 @@
+/*¿Qué devuelve cada expresión?.
+
+Sin probar en la consola, debes adivinar que devolverá cada una de estas expresiones. ¿True o False?
+
+Primera expresión:
+
+let x = 30
+let y = "a"
+y === "b" || x >= 30
+Segunda expresión
+
+let x = 15
+let y = 2
+!( x == "15" || x === y ) && ! (y !== 2 && x <= y)*/
