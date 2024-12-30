431bd728bf3b9bbcac456bd11ba73c2b1692e9b6
Job Pulache <jobpulachecarrenojob837@gmail.com>
2024-12-29 23:40:17 -0500

    ending first exercise

---
+++ variables/01.js
@@ -0,0 +1,9 @@
+/*Vamos a trabajar un poco con la consola para ir amigandonos con ella ya que será nuestra mejor amiga de ahora en más.
+
+Declara una variable llamada nombre en tu consola y asignale tu nombre.
+Declara una variable llamada apellido y asignale como valor tu apellido.
+Muestra por consola las variables nombre y apellido.*/
+
+let nombre = "Job Manu DL"
+let apellido = "Pulache Carreño"
+console.log(`My name is ${nombre} and my lastName is ${apellido}`);
